543226f0dba82031f0b5fb66bab3163db4f7a83f
wanghb <wanghb@lab.rhzz.com.cn>
2020-07-06 18:14:08 +0800

    chore(简单点): 一点点

--- server/app/controller/home.js
+++ server/app/controller/home.js
@@ -5,6 +5,7 @@ const Controller = require('egg').Controller
 class HomeController extends Controller {
   async index() {
     const { ctx } = this
+    console.log('xx')
     ctx.body = 'hi, egg'
   }
 }
